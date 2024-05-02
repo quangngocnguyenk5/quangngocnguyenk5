586f7b4a04ab348405609a7716305ff3abc0b48e
Quang <quangboybin@gmail.com>
2024-05-02 15:33:28 +0700

    initialize

---
+++ src/main/java/org/example/managerapplication/ManagerapplicationApplication.java
@@ -0,0 +1,13 @@
+package org.example.managerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/managerapplication/ManagerapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.managerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
